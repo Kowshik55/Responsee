30fd0b6cf023a9a1e7e1965aef3111af58859ead
Kowshik55 <89611346+Kowshik55@users.noreply.github.com>
2022-04-19 19:07:48 -0500

    Update settings.py

--- pollme/settings.py
+++ pollme/settings.py
@@ -122,8 +122,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/css')]
 
-# Copied from here -STATIC_URL = 'static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
